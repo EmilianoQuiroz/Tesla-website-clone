52e0b25e558dea5a88e8033b916401eb6290edd8
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-02-08 10:50:10 -0300

    Clone de la pagina web de Tesla finalizado

--- nav.js
+++ nav.js
@@ -1,20 +1,19 @@
 const openMenu = () => {
     document.querySelector('.backdrop').className = 'backdrop active';
     document.querySelector('aside').className = 'active';
-}
-const closeMenu = () => {
+  }
+  const closeMenu = () => {
     document.querySelector('.backdrop').className = 'backdrop';
     document.querySelector('aside').className = '';
-}
-
-document.getElementById('menuBtn').onclick = e =>{
+  }
+  
+  document.getElementById('menuBtn').onclick = e => {
     e.preventDefault();
     openMenu();
-}
-
-document.querySelector('aside button.close').onclick = e => {
+  }
+  document.querySelector('aside button.close').onclick = e => {
     closeMenu();
-}
-document.querySelector('.backdrop').onclick = e => {
+  }
+  document.querySelector('.backdrop').onclick = e => {
     closeMenu();
-}
+  }
